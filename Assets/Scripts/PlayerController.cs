e123190dfd7517bca52397e7c22c2b4ba53eec84
Chris Bourke <cbourke@cse.unl.edu>
2020-01-01 10:37:01 -0600

    initial commit
    Just an initial commit of my project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{    
+    private Rigidbody rb;
+    public float force = 1.0f;
+    private int jumpState = 0;
+    private float baseState;
+    private Text countText;
+
+    //TODO: mve this to a GameMetaData class
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+        this.baseState = 0.5f;
+        countText = GameObject.Find("/Canvas/CountText").GetComponent<Text>();
+        this.updateCountText();
+
+
+    }
+
+    // Update is called once per frame
+    //  called before rendering a frame
+    //  seems to be more responsive to "single tap" keyboard input
+    void Update() {
+
+        //reset jump state
+        if (this.rb.position.y <= baseState)
+        {
+            this.jumpState = 0;
+        }
+
+        if (Input.GetKeyDown("space") && jumpState < 1) {
+            //unlimited jumps?
+            this.jumpState++;
+            this.rb.AddForce(0f, 250f, 0f);
+        }
+        else if (Input.GetKeyDown("p") && jumpState == 0)
+        {
+            Debug.Log("hello");
+            this.rb.velocity = new Vector3(0f, this.rb.velocity.y, 0f);
+            this.rb.angularVelocity = Vector3.zero;
+        }
+
+    }
+
+    // Called before any physics calculations
+    void FixedUpdate()
+    {
+        {
+            float moveH = Input.GetAxis("Horizontal");
+            float moveV = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+
+            this.rb.AddForce(movement * this.force);
+
+
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        if( other.gameObject.CompareTag("pickup") )
+        {
+            count++;
+            other.gameObject.SetActive(false);
+            this.updateCountText();
+        }
+    }
+
+    void updateCountText()
+    {
+        this.countText.text = "Count: " + count.ToString();
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Rotator : MonoBehaviour
+{
+    static System.Random R = new System.Random();
+
+    private float speed;
+
+    void Start()
+    {
+        this.speed = (float) R.NextDouble() * 3f;
+        Debug.Log(this.speed);
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3((float)R.NextDouble() * 150, (float)R.NextDouble() * 300, (float)R.NextDouble() * 450) * Time.deltaTime * this.speed);
+    }
+}
